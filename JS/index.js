f87cfed865392e0cbda36fec5708e30d46e5ebb5
BlackDona <kristamedkova@seznam.cz>
2019-10-22 23:12:50 +0200

    změna menu

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,12 @@
+
 function toggleActive(){ 
     document.getElementById("hamburger").classList.toggle("active");
+    document.getElementById("nav").classList.toggle("active");
+    document.getElementById("sum").classList.toggle("active");
+}
+
+function toggleDeactive(){ 
+    document.getElementById("hamburger").classList.toggle("active");
+    document.getElementById("nav").classList.toggle("active");
+    document.getElementById("sum").classList.toggle("active");
 }
